f806f070de740b0fedee69c5e741f2e8321f5474
cloner174 <hamedhajipour2012@hotmail.com>
2024-06-06 21:51:30 +0330

    add points distance

--- SimpleS/Points/Dim2.py
+++ SimpleS/Points/Dim2.py
@@ -1,12 +1,21 @@
 #in the name of GOD##
-import os
-import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from SimpleS.utils import save_path_generator
-import scipy.ndimage as ndi
-from skimage import img_as_ubyte
-from skimage.morphology import skeletonize
+
+
+def calculate_distance_between_two_points(A, B):
+    """
+    Calculate the distance of two points using formula derived from the Pythagorean theorem.
+    Args:
+    A, B (tuple): Points. A = (x1, y1), B = (x2, y2)
+    Returns:
+    float: Distance of the points.
+    """
+    x1, y1 = A
+    x2, y2 = B
+    # Distance = sqrt( (x2-x1)^2 + (y2-y1)^2 )
+    return np.sqrt( ( (x2 - x1)**2 + (y2 - y1)**2 ) )
 
 
 def calculate_midpoint(A, B):
@@ -71,67 +80,4 @@ def create_flat_image(size=(750, 750), color_mode='RGB', color = 'White',return_
                 return image
         else:
                 return
-
-
-def detect_centerline(image,
-                                thrhold1 = 7,
-                                thrhold2 = 255,
-                                c_map ='gray',
-                                fig_size =(6, 6),
-                                plt_title ='Medial Axis',
-                                plt_axis=True,
-                                show = True,
-                                save=False,
-                                save_path = None,
-                                silently = False, 
-                                just_return_medial = False):
-        if isinstance(image, str):
-                img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
-                if img is None:
-                        print("Error loading image")
-                        return
-        try:
-                _, binary = cv2.threshold(image, thrhold1, thrhold2, cv2.THRESH_BINARY)
-                binary = binary > 0
-                skeleton = skeletonize(binary)
-        except:
-                try:
-                        temp = ndi.distance_transform_edt(image)
-                        _, binary = cv2.threshold(temp, thrhold1, thrhold2, cv2.THRESH_BINARY)
-                        binary = binary > 0
-                        skeleton = skeletonize(binary)
-                except:
-                        try:
-                                skeleton = skeletonize(img)
-                        except:
-                                print("Error reading image")
-                                print("Nothing worked!")
-                                return
-        skeleton = img_as_ubyte(skeleton)
-        if just_return_medial:
-                return skeleton
-        else:
-                pass
-        plt.figure(figsize=fig_size)
-        plt.imshow(skeleton, cmap=c_map) if show else None
-        plt.title(plt_title) if show else None
-        if plt_axis:
-                plt.axis('off')
-        if save:
-                if save_path:
-                        if not save_path.endswith(('.png' ,'.jpg')):
-                                save_path = os.path.join(save_path, f'{plt_title}_{thrhold1}_{c_map}.jpg')
-                        else:
-                                pass
-                else:
-                        save_path = f'{plt_title}_{thrhold1}_{c_map}.jpg'
-                plt.imsave(save_path,skeleton, cmap=c_map)
-        if show:
-                plt.show()
-        else:
-                if save and not silently:
-                        print(f"Result is Saved in {save_path}")
-                        return
-                else:
-                        return
 #end#
